8af5b4b8dcadd19f0c657030d23890e1dbe3992e
Suman Maharjan <somonme@gmail.com>
2020-02-20 07:11:28 +0545

    [Test] Separated Positive cases and Negative cases and Added Invalid test case scenario for Palindrome test

--- src/main/java/Palindrome/Test/Palindrome.java
+++ src/main/java/Palindrome/Test/Palindrome.java
@@ -5,9 +5,13 @@ public class Palindrome {
 	public boolean isPalindrome(String originalInput) {
 		String reverseInput = "";
 		
+		if (originalInput == null || originalInput.isEmpty() || originalInput.contains(" ")) {
+			return false;
+		}
 		for (int i = originalInput.length()-1; i>=0; i--) {
 			reverseInput = reverseInput + originalInput.charAt(i);
 		}
+		
 		if (originalInput.equals(reverseInput)) {
 			return true;
 		}else {
--- src/test/java/Palindrome/Test/UnitTest.java
+++ src/test/java/Palindrome/Test/UnitTest.java
@@ -6,8 +6,22 @@ import org.junit.Test;
 public class UnitTest extends Palindrome {
 
 	@Test
-	public void PalindromeTest() {
+	public void PalindromePositiveTest() {
 		Assert.assertTrue(isPalindrome("11011"));
+		Assert.assertTrue(isPalindrome("abcba"));
+	}
+	
+	@Test
+	public void PalindromeNegativeTest() {
 		Assert.assertFalse(isPalindrome("110111"));
+		Assert.assertFalse(isPalindrome("suman"));
+	}
+	
+	@Test
+	public void PalindromeInvalidInputTest() {
+		String NullValue = null;
+		Assert.assertFalse(isPalindrome(NullValue));
+		Assert.assertFalse(isPalindrome(""));
+		Assert.assertFalse(isPalindrome("ab ba"));
 	}
 }
